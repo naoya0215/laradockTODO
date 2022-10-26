dbddfa84635c96ebb46e91fd2607fa633f42bc87
naoya0215 <purgatory0215@icloud.com>
2022-10-26 22:13:56 +0900

    code

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+require __DIR__.'/auth.php';
+
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/folders/create', [App\Http\Controllers\FolderController::class, 'showCreateForm'])->name('folders.create');
+    Route::post('/folders/create', [App\Http\Controllers\FolderController::class, 'create']);
+
+
+    Route::group(['middleware' => 'can:view,folder'], function() {
+        Route::get('/folders/{folder}/tasks', [App\Http\Controllers\TaskController::class, 'index'])->name('tasks.index');
+        Route::get('/folders/{folder}/tasks/create', [App\Http\Controllers\TaskController::class, 'showCreateForm'])->name('tasks.create');
+        Route::post('/folders/{folder}/tasks/create', [App\Http\Controllers\TaskController::class, 'create']);
+        Route::get('/folders/{folder}/tasks/{task}/edit', [App\Http\Controllers\TaskController::class, 'showEditForm'])->name('tasks.edit');
+        Route::post('/folders/{folder}/tasks/{task}/edit', [App\Http\Controllers\TaskController::class, 'edit']);
+    });
+});
